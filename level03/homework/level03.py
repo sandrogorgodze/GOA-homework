ab928f2c44609c115782732df1c3064b4b676f80
sandrogorgodze <sandrogorgodze25@gmail.com>
2024-06-03 20:14:15 +0400

    Add files via upload

---
+++ level03/homework/level03.py
@@ -0,0 +1,25 @@
+
+#info about me
+Name = (" sandro ")
+last_name = (" gorgodze ")
+age = (" 12 ")
+a = (" programing ")
+country = (" Georgia ")
+
+print(Name + last_name + age + a + country)
+
+#do 10 math example
+print(24+36)
+print(632+43)
+print(40-29)
+print(1469-1365)
+print(12*6)
+print(45*3)
+print(40//5)
+print(142//2)
+print(10**5)
+print(8**5)
+
+
+
+
