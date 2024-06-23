83f8de19eb7c79cf92f064f8388c1fc14e959dd2
sandrogorgodze <sandrogorgodze25@gmail.com>
2024-06-23 17:46:49 +0400

    Add files via upload

---
+++ level07/homework/lesson07.py
@@ -0,0 +1,37 @@
+int(input("enter your number"))
+int(input("enter your number"))
+int(input("enter your number"))
+int(input("enter your number"))
+print(5*4)
+print(7-2)
+print(8+5)
+print(10**5)
+print(7//3)
+
+age = (input("enter your age"))
+print("viqnebi 32 20 wlis shemdeg")
+age = (input("enter your age"))
+print("dedachemi iqneba 56 20 wlis shemdeg")
+age = (input("enter your age"))
+print("mamachemi iqneba 60 20 wlis shemdeg")
+age = (input("enter your age"))
+print("chemi dzma iqneba 29 20 wlis shemdeg")
+age = (input("enter your age"))
+print("chemi dzma iqneba 27 20 wlis shemdeg")
+
+name = "sandro"
+surname = "gorgodze"
+age = 12
+country = "gerogia" 
+region = "gardabani"
+color = "black and white"
+car = "bmw,merceds,lambo and ferari"
+food = "lobiani and salad with chese"
+sport = "box and basketball"
+
+print("my name is sandro + surname + gorgodze + age + 12 + country  georgia + gardabani + color black and white + car bmw,merceds,lambo and ferari + food lobiani and salad with chese + sport box and basketball ")
+
+
+name = (input("enter your name"))
+print("happy birthday usser")
+
