25ec4a98589c0a9c5e8209bcf3e7140c7547066f
sandrogorgodze <sandrogorgodze25@gmail.com>
2024-07-10 11:35:51 +0400

    Add files via upload

---
+++ level09/homework/Level09.py
@@ -0,0 +1,55 @@
+print(50 > 40)
+print(37 == 37)
+print(34 < 57)
+print(30 >= 30)
+print(58 != 30)
+print(73 < 93)
+print(63 > 62)
+print(25 != 38)
+print(37 == 37)
+print(64 != 38)
+print(98 > 94)
+print(73 < 95)
+print(84 == 84)
+print(94 < 404)
+print(53 < 303)
+print(83 == 83)
+print(1 < 99)
+print(93 > 16)
+print(1 == 1)
+print(345 != 344)
+print(45 == 45)
+print(67 > 12)
+print(84 != 94)
+print(0 ==  0)
+print(23 > 22)
+print(64 < 74)
+print(54 > 34)
+print(34 != 30)
+print(101 < 102)
+print(23 == 23)
+
+
+
+
+
+
+
+
+
+
+age = 40
+age = 37
+
+
+int(input("enter your age"))
+int(input("enter your age"))
+
+print(40 > 37)
+print("40 წლის ადამიანი დიდი წლოვანებით 37 წლის ადამიანზე")
+
+
+
+
+
+
