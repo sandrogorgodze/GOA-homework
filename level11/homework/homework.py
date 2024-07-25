554b07a3d38861a9230391b2e1cf05102bb864b1
sandrogorgodze <sandrogorgodze25@gmail.com>
2024-07-25 20:45:35 +0400

    Add files via upload

---
+++ level11/homework/homework.py
@@ -0,0 +1,37 @@
+#დავალება: მომხმარებელს შემოატანინეთ სახელი თუ მისი სახელი თუ უდრის თქვენ სახელს მაშინ გამოგვიტანოს True ხოლო სხვა შემთხვევაში False
+user_name = input("enter your name:")
+my_name = "sandro"
+if user_name == my_name:
+    print(True)
+else:
+    print(False)
+
+#მომხმარებელს შემატანინეთ რიცხვი თუ რიცხვი იყო 0 მეტი და 10 ნაკლები მაშინ დაგვიპრინტოს True ხოლო სხვა შემთხვევაში False
+num = int(input("enter your number"))
+if num > 10 and 0:
+    print(True)
+
+else:
+    print(False)
+ 
+#შევესრულოთ შედარებითი მაგალითები
+print(75 == 75)
+print(93 != 63)
+print(73 > 35)
+print(83 < 93)
+print(36 == 36)
+print(73 == 73)
+print(98 > 97)
+print(63 < 86)
+print(42 != 74)
+print(83 > 38)
+print(93 < 94)
+print(74 > 45)
+print(84 < 86)
+print(84 > 45)
+print(74 != 44)
+print(4 > 3)
+print(76 == 76)
+print(83 != 84)
+print(90 > 89)
+print(76 == 76)
