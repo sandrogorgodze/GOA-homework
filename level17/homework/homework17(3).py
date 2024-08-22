afa3e990eec918fd02d3ff39d7487f616efe0f7c
sandrogorgodze <sandrogorgodze25@gmail.com>
2024-08-22 19:30:10 +0400

    Add files via upload

---
+++ level17/homework/homework17(3).py
@@ -0,0 +1,31 @@
+for i in range(0,20 + 1,2):
+    print(i)
+
+for i in range(20,100,5):
+    print(i)
+
+for i in range(0,100 + 1,10):
+    print(i)
+
+for i in range(0,20 +1,4):
+    print(i)
+
+for i in range(0,100 + 1,1):
+    print(i)
+
+for i in range(0,100 + 1,2):
+    print(i)
+
+for i in range(0,60 + 1,20 ):
+    print(i)
+
+for i in range(10,30 + 1,3):
+    print(i)
+
+for i in range(0,10 + 1,1):
+    print(i)
+
+for i in range(0,100 + 1,10):
+    print(i)
+
+    
