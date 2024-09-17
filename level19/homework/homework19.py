d1f56bf0609afb8d570e421ed6c7053a6a92c126
sandrogorgodze <sandrogorgodze25@gmail.com>
2024-09-17 20:28:56 +0400

    Add files via upload

---
+++ level19/homework/homework19.py
@@ -0,0 +1,27 @@
+# for i in range():
+  #  print(i)
+
+#i = 0
+
+#while i <= 10:
+ #    print('Hello')
+  #   i += 1 
+
+
+int(input("დაწერეთ თქვენი ასაკი"))
+
+if 20 <= 18:
+ 
+ print("შენ ხარ სრულწროვანი")
+
+else:
+  
+while 18 <= 0:
+  
+ print("შენ ხარ  არასრულწროვანი")
+    
+
+
+
+
+ 
