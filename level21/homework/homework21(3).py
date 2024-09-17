656f45d1c71ce9c9fb38d1b86279dcec7833f22b
sandrogorgodze <sandrogorgodze25@gmail.com>
2024-09-17 20:09:18 +0400

    Add files via upload

---
+++ level21/homework/homework21(3).py
@@ -0,0 +1,8 @@
+supported = ["lights off","lock the door","make coffee","shut down"]
+not_supported = [ "open the door",]
+(input("what would u do?"))
+if not_supported:   
+   print("unknow")
+else:
+   supported
+   print("ok")
---
+++ level21/homework/homework21.py
@@ -0,0 +1,2 @@
+alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+print(alphabets[8] + " " + alphabets[11] + alphabets[14] + alphabets[21] + alphabets[4] + " " + alphabets[6] + alphabets[14] + alphabets[0])
