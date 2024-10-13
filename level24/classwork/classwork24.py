3461673374612232c7388a4deff82391c5759772
sandrogorgodze <sandrogorgodze25@gmail.com>
2024-10-13 18:26:14 +0400

    Add files via upload

---
+++ level24/classwork/classwork24.py
@@ -0,0 +1,4 @@
+list = [8,4,2,6]
+list.remove(2)
+print(len(list)+list.count(6))
+
---
+++ level24/classworl24.py
@@ -0,0 +1,9 @@
+temperatures = [72,68,75,70,78,74,71]
+print(max(temperatures))
+print(min(temperatures))
+print(sum(temperatures))
+
+
+
+above_70 = [72,75,78,74,71,84,81,73]
+print(above_70)
